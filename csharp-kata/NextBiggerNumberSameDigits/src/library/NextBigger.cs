79e9e93eed27e333d441fbcc1e290db9480e7a1c
Juha Viljanen <juha.o.viljanen@gmail.com>
2017-02-11 13:43:38 +0200

    Next bigger number with same digits in C#

---
+++ csharp-kata/NextBiggerNumberSameDigits/src/library/NextBigger.cs
@@ -0,0 +1,48 @@
+﻿// Kata from here: https://www.codewars.com/kata/55983863da40caa2c900004e
+
+using System;
+using System.Linq;
+
+namespace NextBiggerLibrary
+{
+    public class NextBigger
+    {
+        public int Get(int number)
+        {
+            var digitsReverse = ToDigits(number).Reverse().ToArray();
+            for (int i = 0; i < digitsReverse.Length; i++)
+            {
+                var digitsSoFar = ( i + 1 <= digitsReverse.Length ) ? 
+                    digitsReverse.Take(i+1).ToArray() : digitsReverse;
+                
+                if( digitsReverse[i] < digitsSoFar.Max())
+                {
+                    var sameBeginning = string.Join("", digitsReverse.Skip(i+1).Reverse());
+
+                    var nextBiggest = NextBiggestDigitFromArray(digitsReverse[i], digitsSoFar.ToArray());
+
+                    var nextBiggestIndex = Array.IndexOf(digitsSoFar, nextBiggest);
+                    var newEndingArray = digitsSoFar.
+                        Where( (digit, index) => index != nextBiggestIndex ).ToArray();
+                    Array.Sort(newEndingArray);
+                    var newEnding = string.Join("", newEndingArray);
+
+                    return int.Parse($"{sameBeginning}{nextBiggest}{newEnding}");
+                }
+            }
+            return -1;
+        }
+
+        private int NextBiggestDigitFromArray(int digit, int[] array)
+        {
+            Array.Sort(array);
+            return Array.Find(array, listDigit => listDigit > digit);
+        }
+
+        private int[] ToDigits(int number)
+        {
+            return number.ToString().ToCharArray().
+                Select( digit => int.Parse($"{digit}") ).ToArray();
+        }
+    }
+}
---
+++ csharp-kata/NextBiggerNumberSameDigits/test/test-library/NextBiggerTests.cs
@@ -0,0 +1,50 @@
+﻿using NextBiggerLibrary;
+using Xunit;
+
+namespace TestApp
+{
+    public class NextBiggerTests
+    {
+        [Fact]
+        public void TestGetWithOneDigitNumberShouldAlwaysReturnMinusOne() 
+        {
+            Assert.Equal(-1, new NextBigger().Get(9));
+            Assert.Equal(-1, new NextBigger().Get(1));
+            Assert.Equal(-1, new NextBigger().Get(4));
+        }
+
+        [Fact]
+        public void TestBiggestNumberFromDigitsShouldReturnMinusOne() {
+            Assert.Equal(-1, new NextBigger().Get(21));
+            Assert.Equal(-1, new NextBigger().Get(99));
+            Assert.Equal(-1, new NextBigger().Get(531));
+            Assert.Equal(-1, new NextBigger().Get(987654321));
+            Assert.Equal(-1, new NextBigger().Get(1111111));
+        }
+
+        [Fact]
+        public void TestGetWithTwoDigits()
+        {
+            Assert.Equal(21, new NextBigger().Get(12));
+            Assert.Equal(83, new NextBigger().Get(38));
+        }
+
+        [Fact]
+        public void TestGetWithNDigits()
+        {
+            Assert.Equal(132, new NextBigger().Get(123));
+            Assert.Equal(213, new NextBigger().Get(132));
+            Assert.Equal(2071, new NextBigger().Get(2017));
+            Assert.Equal(2170, new NextBigger().Get(2107));
+            Assert.Equal(12345687, new NextBigger().Get(12345678));
+            Assert.Equal(13245, new NextBigger().Get(12543));
+        }
+
+        [Fact]
+        public void TestGetWithSeveralSameDigits()
+        {
+            Assert.Equal(2122, new NextBigger().Get(1222));
+            Assert.Equal(133353, new NextBigger().Get(133335));
+        }
+    }
+}
