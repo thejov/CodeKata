b4b0f7521e94dc5f7681cc57aa3f41039832632d
thejov <juha.o.viljanen@gmail.com>
2017-03-08 22:02:14 +0200

    Switch to idiomatic JS casing

--- javascript-kata/BinaryChop/src/karate_chop.js
+++ javascript-kata/BinaryChop/src/karate_chop.js
@@ -3,19 +3,19 @@
 
 'use strict';
 
-exports.chop = (searched_number, array_of_numbers) => {
-    if (array_of_numbers.length === 0) return -1;        
+exports.chop = (searchedNumber, arrayOfNumbers) => {
+    if (arrayOfNumbers.length === 0) return -1;        
 
-    const middle_index = Math.floor(array_of_numbers.length / 2);
-    const middle_item = array_of_numbers[middle_index];
+    const MIDDLE_INDEX = Math.floor(arrayOfNumbers.length / 2);
+    const MIDDLE_ITEM = arrayOfNumbers[MIDDLE_INDEX];
 
-    if (searched_number === middle_item) {
-        return middle_index;
-    } else if (searched_number < middle_item) {
-        return this.chop(searched_number, array_of_numbers.slice(0, middle_index));
+    if (searchedNumber === MIDDLE_ITEM) {
+        return MIDDLE_INDEX;
+    } else if (searchedNumber < MIDDLE_ITEM) {
+        return this.chop(searchedNumber, arrayOfNumbers.slice(0, MIDDLE_INDEX));
     } else {
-        const index = this.chop(searched_number, array_of_numbers.slice(middle_index + 1));
-        return index === -1 ? index : middle_index + index + 1;
+        const index = this.chop(searchedNumber, arrayOfNumbers.slice(MIDDLE_INDEX + 1));
+        return index === -1 ? index : MIDDLE_INDEX + index + 1;
     }
         
 };
--- javascript-kata/BinaryChop/test/karate_chop_test.js
+++ javascript-kata/BinaryChop/test/karate_chop_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const karate_chop = require('../src/karate_chop.js');
+const karateChop = require('../src/karate_chop.js');
 const range = require('lodash.range');
 
 describe('karate_chop', () => {
@@ -9,42 +9,42 @@ describe('karate_chop', () => {
   describe('#chop()', () => {
 
     it('should return -1 when given empty array', () => {
-      assert.equal( -1, karate_chop.chop(1, []) );
+      assert.equal( -1, karateChop.chop(1, []) );
     });
 
     it('should return 0 when number found in array of size 1', () => {
-      assert.equal( 0, karate_chop.chop(1, [1]) );
+      assert.equal( 0, karateChop.chop(1, [1]) );
     });
 
     it('should return -1 when number not found in array', () => {
-      assert.equal( -1, karate_chop.chop(2, [1]) );
-      assert.equal( -1, karate_chop.chop(0, [1, 3, 5]) );
-      assert.equal( -1, karate_chop.chop(2, [1, 3, 5]) );
-      assert.equal( -1, karate_chop.chop(4, [1, 3, 5]) );
-      assert.equal( -1, karate_chop.chop(6, [1, 3, 5]) );
-      assert.equal( -1, karate_chop.chop(0, [1, 3, 5, 7]) );
-      assert.equal( -1, karate_chop.chop(2, [1, 3, 5, 7]) );
-      assert.equal( -1, karate_chop.chop(4, [1, 3, 5, 7]) );
-      assert.equal( -1, karate_chop.chop(6, [1, 3, 5, 7]) );
-      assert.equal( -1, karate_chop.chop(8, [1, 3, 5, 7]) );
+      assert.equal( -1, karateChop.chop(2, [1]) );
+      assert.equal( -1, karateChop.chop(0, [1, 3, 5]) );
+      assert.equal( -1, karateChop.chop(2, [1, 3, 5]) );
+      assert.equal( -1, karateChop.chop(4, [1, 3, 5]) );
+      assert.equal( -1, karateChop.chop(6, [1, 3, 5]) );
+      assert.equal( -1, karateChop.chop(0, [1, 3, 5, 7]) );
+      assert.equal( -1, karateChop.chop(2, [1, 3, 5, 7]) );
+      assert.equal( -1, karateChop.chop(4, [1, 3, 5, 7]) );
+      assert.equal( -1, karateChop.chop(6, [1, 3, 5, 7]) );
+      assert.equal( -1, karateChop.chop(8, [1, 3, 5, 7]) );
     });
 
     it('should return index of searched number for an array with n elements', () => {      
-      assert.equal( 0, karate_chop.chop(1, [1, 3, 5]) );
-      assert.equal( 1, karate_chop.chop(3, [1, 3, 5]) );
-      assert.equal( 2, karate_chop.chop(5, [1, 3, 5]) );
+      assert.equal( 0, karateChop.chop(1, [1, 3, 5]) );
+      assert.equal( 1, karateChop.chop(3, [1, 3, 5]) );
+      assert.equal( 2, karateChop.chop(5, [1, 3, 5]) );
 
-      assert.equal( 0, karate_chop.chop(1, [1, 3, 5, 7]) );
-      assert.equal( 1, karate_chop.chop(3, [1, 3, 5, 7]) );
-      assert.equal( 2, karate_chop.chop(5, [1, 3, 5, 7]) );
-      assert.equal( 3, karate_chop.chop(7, [1, 3, 5, 7]) );
+      assert.equal( 0, karateChop.chop(1, [1, 3, 5, 7]) );
+      assert.equal( 1, karateChop.chop(3, [1, 3, 5, 7]) );
+      assert.equal( 2, karateChop.chop(5, [1, 3, 5, 7]) );
+      assert.equal( 3, karateChop.chop(7, [1, 3, 5, 7]) );
     });
 
     it('should return index of searched number for a huge array', () => {
-      let huge_array = range(1, 100001);
-      assert.equal( 0, karate_chop.chop(1, huge_array) );
-      assert.equal( 99999, karate_chop.chop(100000, huge_array) );
-      assert.equal( 998, karate_chop.chop(999, huge_array) );
+      let hugeArray = range(1, 100001);
+      assert.equal( 0, karateChop.chop(1, hugeArray) );
+      assert.equal( 99999, karateChop.chop(100000, hugeArray) );
+      assert.equal( 998, karateChop.chop(999, hugeArray) );
     });
 
   });
